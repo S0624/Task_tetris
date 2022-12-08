8695605d2e8a04a5c7e389fa70ff09b51076cdb6
大野 栞穂 <fko2247078@stu.o-hara.ac.jp>
2022-12-08 12:32:54 +0900

    置けるようにする処理の追加

--- Task_tetris/Scene/SceneMain.cpp
+++ Task_tetris/Scene/SceneMain.cpp
@@ -14,11 +14,12 @@ namespace
 	constexpr int kFieldFloor = 570;			//fieldの下
 
 	constexpr int kFrameTimer = 50;
+	constexpr int kMinoTimer = 10;
 
 	int field[kBlocHeight][kBlocWindht];
 	
-	int x = 0;
-	int y = 0;
+	int m_coordinateX = 0;
+	int m_coordinateY = 0;
 
 	const char* kNameTitle = "Main画面";
 	const char* kNameGuide = "未実装";
@@ -29,7 +30,9 @@ SceneMain::SceneMain():
 	m_pos(),									//ミノのポジション
 	m_size(),									//ミノのサイズ
 	m_gravity(),								//ミノの重力
-	m_speed()									//ミノの落下スピード
+	m_speed(),									//ミノの落下スピード
+	m_placed(false),
+	m_minotimer()
 {
 	m_pos.x = 150;								//初期位置
 	m_pos.y = 0;								//初期位置
@@ -44,38 +47,36 @@ SceneMain::SceneMain():
 void SceneMain::Init()
 {
 	m_frametimer = kFrameTimer;					//タイマーを設定
+	m_minotimer = kMinoTimer;
 	for (int i = 0; i < kBlocHeight; i++)
 	{
 		for (int j = 0; j < kBlocWindht; j++)
 		{
 			field[j][i] = 0;
-			//field[j][i] = empty;
 		}
 	}
-	
-
-	//for (int i = 0; i < kBlocHeight; i++)
-	//{
-	//	printfDx("\n");
-	//	for (int j = 0; j < kBlocWindht; j++)
-	//	{
-	//		switch (field[j][i])
-	//		{
-	//		case 0:
-	//			printfDx("%d",j);
-	//			break;
-	//		case 1:
-	//			printfDx("8");
-	//		default:
-	//			break;
-	//		}
-	//	}
-	//}
 }
 
 
 void SceneMain::MoveUpdate()
 {
+	m_coordinateY = m_pos.y / m_size.y;
+	m_coordinateX = m_pos.x / m_size.x;
+
+	if (m_pos.y == kFieldFloor)								//
+	{
+		if (m_minotimer > 0)
+		{
+			m_minotimer--;
+			return;
+		}
+		if (m_minotimer <= 0)
+		{
+			m_placed = true;
+			field[m_coordinateY][m_coordinateX] = 1;
+		}
+	}
+
 	if (Pad::isTrigger(PAD_INPUT_LEFT))				//左の移動処理、移動制限
 	{
 		m_pos.x -= m_size.x;						//キーが押されたら押された方向に動く
@@ -87,20 +88,20 @@ void SceneMain::MoveUpdate()
 	if (Pad::isTrigger(PAD_INPUT_RIGHT))			//右の移動処理、移動制限
 	{
 		m_pos.x += m_size.x;						//キーが押されたら押された方向に動く
-		//m_pos.x += m_size.x;						//キーが押されたら押された方向に動く
 		if (m_pos.x > 270)
 		{
 			m_pos.x = 270;
 		}
 	}
 
-	if (m_pos.y > 600)								//fieldから出ないように設定
+	if (m_pos.y > 570)								//fieldから出ないように設定
 	{
-		m_pos.y = 600;
+		m_pos.y = 570;
 	}
 	if (Pad::isTrigger(PAD_INPUT_UP))				//上の移動処理
 	{
-		m_pos.y = kFieldFloor;								//キーが押されたら一瞬で下まで
+		m_pos.y = kFieldFloor;						//キーが押されたら一瞬で下まで
+		m_minotimer = 0;
 	}
 
 	if (Pad::isPress(PAD_INPUT_DOWN))				//下の移動処理
@@ -118,41 +119,42 @@ void SceneMain::MoveUpdate()
 		m_pos.y += m_gravity;
 		m_frametimer = kFrameTimer;					//落下させたらタイマーの数値を元に戻す
 	}
-	if (m_pos.y > kFieldFloor)								//fieldから出ないように設定
-	{
-		m_pos.y = kFieldFloor;
-	}
 
-
-	//////////
-	//y = static_cast<int>(m_pos.y / m_size.y);
-	//x = static_cast<int>(m_pos.x / m_size.x);
-	
-	
-	y = m_pos.y / m_size.y;
-	x = m_pos.x / m_size.x;
-	
-	if (m_pos.y == kFieldFloor)								//
-	{
-		field[19][1] = 1;
-	}
 }
 
 SceneBase* SceneMain::Update()
 {
-	MoveUpdate();									//ミノの移動処理
-
+	if (!m_placed)
+	{
+		MoveUpdate();									//ミノの移動処理
+	}
 	return this;
 }
 void SceneMain::Draw()
 {
-	//DrawBox(kFieldWindth, kFieldHeight + 30, 430, 705, GetColor(255, 255, 255), false);					//fieldの枠
-	//DrawBox(kFieldWindth, kFieldHeight, 430, 705, GetColor(255, 255, 255), false);					//fieldの枠
-	DrawBox(kFieldWindth, kFieldHeight, 300, 600, GetColor(255, 255, 255), false);					//fieldの枠
+	if (Placed())
+	{
+		DrawBox(static_cast<int>(m_pos.x), static_cast<int>(m_pos.y),						//ミノの表示
+			static_cast<int>(m_pos.x + m_size.x), static_cast<int>(m_pos.y + m_size.y),
+			GetColor(255, 0, 0), false);
+	}
 
-	DrawBox(static_cast<int>(m_pos.x + 2), static_cast<int>(m_pos.y + 2),						//ミノの表示
-		static_cast<int>(m_pos.x + m_size.x - 2), static_cast<int>(m_pos.y + m_size.y - 2),
+	if (!Placed())
+	{
+		DrawBox(static_cast<int>(m_pos.x), static_cast<int>(m_pos.y),						//ミノの表示
+		static_cast<int>(m_pos.x + m_size.x), static_cast<int>(m_pos.y + m_size.y),
 		GetColor(0, 255, 255), false);
+	}
+
+	DrawBox(kFieldWindth - 2, kFieldHeight - 2, 300 + 2, 600 + 2, GetColor(255, 255, 255), false);					//fieldの枠
+
+
+
+
+	///////////////////////////////////////
+	///        確認用の数値表示         ///
+	///////////////////////////////////////
+
 
 	for (int i = 0; i < kBlocHeight; i++)
 	{
@@ -163,15 +165,23 @@ void SceneMain::Draw()
 			DrawFormatString(kFieldWindth + (j * 30), kFieldHeight, GetColor(255, 255, 255), " %d", j);
 		}
 	}
-	DrawFormatString(500, 0, GetColor(255, 255, 255), " %d", m_frametimer);
+
+	//DrawFormatString(500, 0, GetColor(255, 255, 255), " %d", m_frametimer);
 	DrawFormatString(600, 0, GetColor(255, 255, 255), " {x %d,y %d}", static_cast<int>(m_pos.x / 30), static_cast<int>(m_pos.y / 30));		//座標
-	DrawFormatString(600, 75, GetColor(255, 255, 255), " {x %d,y %d}", static_cast<int>(x), static_cast<int>(y));		//座標
-	DrawFormatString(600, 100, GetColor(255, 255, 255), " y %d", y);		//座標
+	DrawFormatString(600, 75, GetColor(255, 255, 255), " {x %d,y %d}", static_cast<int>(m_coordinateX), static_cast<int>(m_coordinateY));		//座標
+	DrawFormatString(600, 100, GetColor(255, 255, 255), " y %d", m_coordinateY);		//座標
 	//DrawFormatString(600, 125, GetColor(255, 255, 255), " x %d", x);		//座標
-	
-	///////////////////////////////////////
-	if (field[y][x] == 1)
+
+	if (field[m_coordinateY][m_coordinateX] == 1)
 	{
 		DrawFormatString(600, 50, GetColor(255, 255, 255), " %d", 1);
+	}
+	if (Placed() == true)
+	{
+		DrawFormatString(700, 300, GetColor(255, 255, 255), " a");		//座標
+	}
+	if (Placed() == false)
+	{
+		DrawFormatString(700, 300, GetColor(255, 255, 255), " b");		//座標
 	}
 }
--- Task_tetris/Scene/SceneMain.h
+++ Task_tetris/Scene/SceneMain.h
@@ -20,6 +20,8 @@ public:
 
 	//ミノの移動処理
 	virtual void MoveUpdate();
+	//ミノが置かれたかどうか
+	virtual bool Placed() { return m_placed; }
 	//更新処理
 	virtual SceneBase* Update()override;
 	//描画処理
@@ -27,9 +29,11 @@ public:
 
 private:
 	int m_frametimer;	//フレームタイマー
+	int m_minotimer;	//ミノのタイマー
 	Vec2 m_pos;			//ミノの位置
 	Vec2 m_size;		//ミノのサイズ
 	int m_gravity;		//ミノの重力落下
 	float m_speed;		//押された時のミノのスピード	
+	bool m_placed;		//ミノが置かれたかどうか
 };
 
