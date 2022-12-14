78e14a15426341cc4375b8477af1be5ef642c41f
大野 栞穂 <fko2247078@stu.o-hara.ac.jp>
2022-12-14 15:51:42 +0900

    コメントの追加 一列消す処理 消えたら一段下げる処理

--- Task_tetris/Scene/SceneMain.cpp
+++ Task_tetris/Scene/SceneMain.cpp
@@ -18,10 +18,11 @@ namespace
 
 	int field[kBlocWindht][kBlocHeight];		//fieldの設置
 	
-	int m_coordinateX = 0;
-	int m_coordinateY = 0;
+	int kCoordinateX = 0;						//現在地から座標を取得する
+	int kCoordinateY = 0;						//現在地から座標を取得する
 
-	bool isEnd = false;
+	bool kIsEnd = false;						//ゲームの終了フラグ
+
 }
 
 SceneMain::SceneMain():
@@ -30,8 +31,8 @@ SceneMain::SceneMain():
 	m_size(),									//ミノのサイズ
 	m_gravity(),								//ミノの重力
 	m_speed(),									//ミノの落下スピード
-	m_placed(false),
-	m_minotimer()
+	m_placed(false),							//置かれたかどうか
+	m_minotimer()								//ミノが下まで行って置かれるまでのインターバル
 {
 	MinoInit();
 }
@@ -74,8 +75,8 @@ void SceneMain::MinoInit()
 
 void SceneMain::MoveUpdate()
 {
-	m_coordinateY = (m_pos.y - 25) / m_size.y;
-	m_coordinateX = (m_pos.x - kFieldDisplace) / m_size.x;
+	kCoordinateY = (m_pos.y - 25) / m_size.y;						//ミノの現在地を座標にする
+	kCoordinateX = (m_pos.x - kFieldDisplace) / m_size.x;			//ミノの現在地を座標にする
 
 	if (m_placed == true)								//	ミノの設置処理
 	{
@@ -88,20 +89,20 @@ void SceneMain::MoveUpdate()
 
 	if (Pad::isTrigger(PAD_INPUT_LEFT))				//左の移動処理、移動制限
 	{
-		if (field[m_coordinateX - 1][m_coordinateY] == empty)
+		if (field[kCoordinateX - 1][kCoordinateY] == empty)
 		{
 			m_pos.x -= m_size.x;						//キーが押されたら押された方向に動く
 		}
 	}
 	if (Pad::isTrigger(PAD_INPUT_RIGHT))			//右の移動処理、移動制限
 	{
-		if (field[m_coordinateX + 1][m_coordinateY] == empty)
+		if (field[kCoordinateX + 1][kCoordinateY] == empty)
 		{
 			m_pos.x += m_size.x;
 		}
 	}
 	
-	if (field[m_coordinateX][m_coordinateY + 1] != empty)								//field下から出ないように設定
+	if (field[kCoordinateX][kCoordinateY + 1] != empty)								//field下から出ないように設定
 	{
 		m_placed = true;
 		return;
@@ -109,9 +110,9 @@ void SceneMain::MoveUpdate()
 
 	if (Pad::isTrigger(PAD_INPUT_UP))				//上の移動処理
 	{
-		for (int i = kBlocHeight; i > 0; i--)
+		for (int i = 0; i <= kBlocHeight; i++)
 		{
-			if (field[m_coordinateX][i - 1] == empty)
+			if (field[kCoordinateX][i] != empty)
 			{
 				m_pos.y = i + (i * 25);				//置ける場所のチェック
 				break;
@@ -121,7 +122,7 @@ void SceneMain::MoveUpdate()
 	
 	if (Pad::isPress(PAD_INPUT_DOWN))				//下の移動処理
 	{
-		if (field[m_coordinateX][m_coordinateY + 1] == empty)
+		if (field[kCoordinateX][kCoordinateY] == empty)
 		{
 			m_pos.y += m_speed;							//キーが押されたら押された方向に動く
 		}
@@ -143,8 +144,8 @@ SceneBase* SceneMain::Update()
 {
 	if (m_minotimer <= 0)
 	{
-		field[m_coordinateX][m_coordinateY] = input;
-		m_placed = false;
+		field[kCoordinateX][kCoordinateY] = input;		//置かれたらfieldに代入する
+		m_placed = false;								//フラグをもとに戻す
 		m_minotimer = kMinoTimer;
 		MinoInit();
 	}
@@ -153,21 +154,42 @@ SceneBase* SceneMain::Update()
 	{
 		if (field[j][0] != empty)
 		{
-			isEnd = true;
+			kIsEnd = true;								//ゲームオーバー
 		}
 	}
-	if (isEnd == false)
+	if (kIsEnd == false)
 	{
 		MoveUpdate();									//ミノの移動処理
 	}
-
+	
+	bool disappear = false;
+	int height = 0;
+	for (int i = 1; i < kBlocHeight - 1; i++)
+	{
+		if (field[1][i] == input && field[2][i] == input && field[3][i] == input
+			&& field[4][i] == input && field[5][i] == input && field[6][i] == input
+			&& field[7][i] == input && field[8][i] == input && field[9][i] == input && field[10][i] == input)
+		{
+			disappear = true;
+			height = i;
+		}
+	}
 	for (int j = 1; j < kBlocWindht - 1; j++)
 	{
-		if (field[1][20] == input && field[2][20] == input && field[3][20] == input
-			&& field[4][20] == input && field[5][20] == input && field[6][20] == input
-			&& field[7][20] == input && field[8][20] == input && field[9][20] == input && field[10][20] == input)
+		if(disappear == true)
 		{
-			field[j][20] = empty;
+			field[j][height] = empty;
+		}
+	}
+
+	if (disappear == true)
+	{
+		for (int i = kBlocHeight - 2; i >= 1; i--)
+		{
+			for (int j = 1; j < kBlocWindht - 1; j++)
+			{
+				field[j][i] = field[j][i - 1];
+			}
 		}
 	}
 
@@ -203,12 +225,15 @@ void SceneMain::Draw()
 	///        確認用の数値表示         ///
 	///////////////////////////////////////
 
-	if (isEnd == false)
+	if (kIsEnd == false)
 	{
 		DrawString(500, 0, "GamePlay", GetColor(255, 0, 0));
 	}
-	if (isEnd == true)
+	if (kIsEnd == true)
 	{
 		DrawString(500, 0, "GameOver", GetColor(255, 0, 0));
 	}
+
+	//DrawFormatString(600, 0, GetColor(255, 255, 255), "%d", kCount);
+	//DrawFormatString(650, 0, GetColor(255, 255, 255), "%d", kTotal);
 }
